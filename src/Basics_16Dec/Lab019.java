aa35904f21db28b2ceb93c75f4f232d44f8bff37
Avijeet Jana <112279206+avilogin@users.noreply.github.com>
2023-12-28 20:19:51 +0530

    Create Lab019.java

---
+++ src/Basics_16Dec/Lab019.java
@@ -0,0 +1,15 @@
+package src.Basics_16Dec;
+
+import java.util.Scanner;
+import java.util.SortedMap;
+
+public class Lab018 {
+    public static void main(String[] args) {
+
+       
+        System.out.println("Testing the Pull from github");
+
+       
+    }
+}
+
