ecd829790624b07aca7fef93ad6f11c14d3c9210
avijeet <avijeetjana26@gmail.com>
2023-12-28 20:26:37 +0530

    testing push command

--- src/Basics_16Dec/Lab019.java
+++ src/Basics_16Dec/Lab019.java
@@ -1,15 +1,12 @@
 package src.Basics_16Dec;
 
-import java.util.Scanner;
-import java.util.SortedMap;
-
-public class Lab018 {
+public class Lab019 {
     public static void main(String[] args) {
 
-       
-        System.out.println("Testing the Pull from github");
 
-       
+        System.out.println("Testing Done");
+
+
     }
 }
 
