23f30d5fb8f3e5904dabb753053274a24de56188
avijeet <avijeetjana26@gmail.com>
2024-01-05 00:28:55 +0530

    testing clone of reo

---
+++ src/Basics006_24Dec/Lab054_test.java
@@ -0,0 +1,16 @@
+package src.Basics006_24Dec;
+
+public class Lab054_test {
+    public static void main(String[] args) {
+
+        System.out.println("Testing the repo clone");
+
+
+
+
+
+
+
+
+    }
+}
